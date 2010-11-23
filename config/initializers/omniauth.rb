1153acae434c4e5253013848a11c130fd06854e8
Ethan Fast <ethan@ethan-fasts-macbook-pro.local>
2010-11-23 08:03:37 -0500

    facebook?

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'u1rKrNjTownQZO2KfP4yZw', 'gNIh6myFIjunLGSYLafcu12lSb3fUnqFGIZ71IOw'
+  provider :facebook, '3a8155731436645771c888faee7177f6', 'ec097dcb82bbf26008e1337b52f36625'
 end
